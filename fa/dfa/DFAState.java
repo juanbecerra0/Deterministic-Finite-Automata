fde82a83cc40161d837c4018b3814197556201fe
Juan Becerra <juanbecerra@u.boisestate.edu>
2020-02-21 10:23:40 -0700

    Created DFA classes, added README template

---
+++ fa/dfa/DFAState.java
@@ -0,0 +1,16 @@
+package fa.dfa;
+
+import fa.State;
+
+/**
+ * Class that represents a single state in a deterministic finite automata.
+ * Extends the functionality of the abstract State class. Used by DFA instances 
+ * to represent the IO nature of states in finite state machines.
+ * 
+ * @author Juan Becerra
+ * @author Keegan Saunders
+ */
+public class DFAState extends State {
+    // TODO add methods needed for State class
+
+}
